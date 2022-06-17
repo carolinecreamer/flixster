edd192d46bacea3bf6856dc57128e1ebbc494fd1
Caroline Creamer <ccreamer@fb.com>
2022-06-17 13:58:03 -0700

    search bar fix

--- script.js
+++ script.js
@@ -7,6 +7,35 @@ let pages       = 1;
 let limit       = 10;
 let offset      = pages * limit;
 
+
+const clearIcon  = document.querySelector(".clear-icon");
+const searchIcon = document.querySelector(".search-icon");
+const searchBar  = document.getElementById("search");
+
+searchBar.addEventListener("keyup", (event) => {
+    if(searchBar.value == "") {
+        clearIcon.style.visibility  = "hidden";
+        console.log("here")
+        document.getElementById('results').innerHTML = ``;
+        fetchMovies(false);
+    } else if(searchBar.value){
+        clearIcon.style.visibility   = "visible";
+        searchParam = document.getElementById('search').value;
+        pages = 1;
+        document.getElementById('results').innerHTML = ``;
+        fetchMovies(true);
+    } 
+});
+
+clearIcon.addEventListener("click", () => {
+    document.getElementById('results').innerHTML = ``
+    searchBar.value = "";
+    clearIcon.style.visibility = "hidden";
+    console.log("click")
+    fetchMovies(false);
+})
+
+
 function loadResults(search) {
     ++pages;
     //offset = pages * limit;
@@ -23,14 +52,13 @@ function renderMovies(search) {
     console.log(movieData)
     movieData.results.forEach((row, index) => {
         document.getElementById('results').innerHTML += `
-        <div>
-            <div >
+        <div class="imageContainer">
+            <div class="movieContainer">
                 <img class="movie" src="https://image.tmdb.org/t/p/w500/${movieData.results[index].poster_path}" >
             </div>
-            <div class="nogrid">
-                <p>${movieData.results[index].title}</p>
-                <div class="text"> 
-                    
+            <div class="moveTitle">
+                <p class="text end bold">${movieData.results[index].title}</p>
+                <div class="text start"> 
                     <p><img class="star" src="1200px-Gold_Star.svg.png">  ${movieData.results[index].vote_average}</p>
                 </div>
             </div>
@@ -48,19 +76,18 @@ function renderMovies(search) {
 
 async function fetchMovies(search) {
     try {
-        var response;
         if (search == true) {
             console.log("search")
-            response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&page=${pages}&include_adult=false&query=${searchParam}`)
-            const results  = await response.json(); 
+            var response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&page=${pages}&include_adult=false&query=${searchParam}`)
+            var results  = await response.json(); 
             console.log(results)
             movieData = results;
             renderMovies(true);
         }
         else {
-            console.log("notSearch")
-            response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=${pages}`)
-            const results  = await response.json(); 
+            console.log(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=${pages}`)
+            var response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=${pages}`)
+            var results  = await response.json(); 
             console.log(results)
             movieData = results;
             renderMovies(false);
@@ -72,7 +99,7 @@ async function fetchMovies(search) {
     }
 }
 
-document.getElementById('form').addEventListener('submit', (event) => {
+/*document.getElementById('form').addEventListener('submit', (event) => {
     event.preventDefault();
     searchParam = document.getElementById('search').value;
     console.log(searchParam)
@@ -80,7 +107,7 @@ document.getElementById('form').addEventListener('submit', (event) => {
     document.getElementById('results').innerHTML = ``;
     fetchMovies(true);
 })
-
+*/
 window.onload = function () {
     console.log("here")
     fetchMovies(false);
